91508c64b060c1578e9cec75df64b3f26973bc64
Kristján Pétursson <kristjan@gmail.com>
2016-06-19 15:14:33 -0700

    Perform full search for N-1 addends

--- like_powers.rb
+++ like_powers.rb
@@ -1,45 +1,73 @@
 #!/usr/bin/env ruby
 
+require 'pqueue'
+
 # Disprove Euler's conjecture on sums of like powers.
 # In 1966, finding the smallest example took a computer the size of  a van.
 # http://fermatslibrary.com/s/counterexample-to-eulers-conjecture-on-sums-of-like-powers
 
-MAX_ADDER = 150
-MAX_BASE = 150
-MAX_POWER = 6
-
 def format(set, base, power)
-  set.map { |i| "#{i}^#{power}" }.join(' + ') + " = #{base}^#{power}"
+   "(#{base ** power}) #{base}^#{power} = " + set.map { |i| "#{i}^#{power}" }.join(' + ')
 end
 
-def increment(set, max, skip: false)
-  set.each.with_index do |n, i|
-    if n == max || skip
-      skip = false
-      set[i] = 1
-    else
+def add_targets(queue, target)
+  base_change  = target.merge(base: target[:base] + 1)
+  power_change = target.merge(power: target[:power] + 1)
+  queue << base_change  unless queue.include?(base_change)
+  queue << power_change unless queue.include?(power_change)
+end
+
+def search(base, power)
+  tries = 0
+  target = base ** power
+  puts "Searching for #{base}^#{power}\t(#{target})"
+
+  set = Array.new(power - 1) { 1 }
+
+  loop do
+    tries += 1
+    #puts "\t#{tries}\t#{format(set, base, power)}"
+
+    addends = set.map { |i| i ** power }
+    sum = addends.inject(:+)
+    return { tries: tries, set: set } if sum == target # Found one!
+
+    # Increment the working set with rollover as each element reaches the target
+    i = 0
+    while i < set.size
       set[i] += 1
-      (i - 1).downto(0).each {|j| set[j] = set[j + 1] }
-      break
+      break if set[i] < base
+      i += 1
+    end
+    # Every element is base ^ power
+    return { tries: tries, set: nil } if i == set.size
+
+    # IF we just did [1, 1] then [2, 1], we don't need to try [1, 2].
+    # Skip straight to [2, 2].
+    while i > 0
+      set[i - 1] = set[i]
+      i -= 1
     end
   end
 end
+
+
+target_queue = PQueue.new { |a, b| b[:base] ** b[:power] <=> a[:base] ** a[:power] }
+target_queue << { base: 2, power: 3 }
 
 tries = 0
-5.upto(MAX_POWER) do |power|
-  144.upto(MAX_BASE) do |base|
-    set = Array.new(power - 1) { 1 }
-    total = base ** power
-    until set.all? { |i| i == MAX_ADDER }
-      sum = set.map { |i| i ** power }.inject(:+)
-      case
-      when sum == total
-        puts tries, format(set, base, power)
-        exit
-      when sum > total then increment(set, MAX_ADDER, skip: true)
-      else increment(set, MAX_ADDER)
-      end
-      tries += 1
-    end
+potential_targets = 0
+loop do
+  target = target_queue.pop
+  potential_targets += 1
+
+  result = search(target[:base], target[:power])
+
+  tries += result[:tries]
+  if result[:set]
+    puts "Done! #{tries} tries. #{potential_targets} targets. #{format(result[:set], target[:base], target[:power])}"
+    exit
   end
+
+  add_targets(target_queue, target)
 end
